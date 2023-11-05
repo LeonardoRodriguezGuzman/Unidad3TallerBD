aa323adcabfa068db810679a7d40586686966b01
Leonardo Rodríguez Guzmán <s18120163@alumnos.itsur.edu.mx>
2023-11-05 02:03:23 -0600

    Ya conecta con Azure

---
+++ Conexion/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+    public class Conexion
+    {
+        private string cadenaConexion;
+        private MySqlConnection conexion;
+        private string user;
+        private string password;
+
+        public Conexion(string user, string password)
+        {
+            cadenaConexion = "Server=s18120163.mysql.database.azure.com;Database=mydb;User=" + user+";Password="+password+";";
+            conexion = new MySqlConnection(cadenaConexion);
+        }
+        public MySqlConnection AbrirConexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Closed)
+                {
+
+                MessageBox.Show("se armo");
+                    conexion.Open();
+                }
+            }
+            catch (MySqlException ex)
+            {
+            // Manejar excepciones en caso de problemas de conexión
+                MessageBox.Show("Error de conexión: " + ex.Message);
+            }
+
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
--- Login.cs
+++ Login.cs
@@ -6,5 +6,12 @@ namespace TallerBD
         {
             InitializeComponent();
         }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion(txtUser.Text, txtPassword.Text);
+            con.AbrirConexion();
+            con.CerrarConexion();
+        }
     }
 }
